5a7174af3bae49fa3d1e7670f8cded409fc3e10d
Anna <maedchenkunst@gmail.com>
2017-10-18 01:59:44 +0900

    some fixing again

--- js/data.js
+++ js/data.js
@@ -130,10 +130,10 @@
              var userData = firebase.database().ref("tag/languages/" + ladded.key + "/" + userID).set(true);
          });
          firebase.database().ref("users/" + userID + "/hobbies").on("child_removed", function (hbremoved) {
-             var userData = firebase.database().ref("tag/hobbies/" + hbremoved.key + "/" + userID).set(true);
+             var userData = firebase.database().ref("tag/hobbies/" + hbremoved.key + "/" + userID).set(null);
          });
          firebase.database().ref("users/" + userID + "/languages").on("child_removed", function (lremoved) {
-             var userData = firebase.database().ref("tag/languages/" + lremoved.key + "/" + userID).set(true);
+             var userData = firebase.database().ref("tag/languages/" + lremoved.key + "/" + userID).set(null);
          });
          userData.update({
                  name: form.uname.value,
